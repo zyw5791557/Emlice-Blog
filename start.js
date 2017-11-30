943afe9164c879a6d54151998abb5f2a9ddf5caa
Amo <xiaomozyw@sina.com>
2017-11-30 11:14:21 +0800

    配置自动检测启动文件 start.js
    Signed-off-by: Amo <xiaomozyw@sina.com>

--- start.js
+++ start.js
@@ -1,7 +1,29 @@
-var process = require('child_process');
+var { exec } = require('child_process');
 
-process.exec('npm start', function (err,stdout,stderr) {
+exec('npm start', function (err,stdout,stderr) {
 	if(err) {
 		console.log('exec error:' + err);
 	}
-})
+});
+
+/*
+* setInterval git pull
+* @getTimer 时 * 分 * 秒 * 毫秒
+*/
+
+let git;
+const gitTimer = 1 * 60 * 60 * 1000;
+setInterval(() => {
+    git = exec(
+        'git reset --hard && git pull',
+        (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+              }
+              console.log('Standard Output:', stdout);
+              console.log('Stderr Output:', stderr);
+        }
+    );
+    
+},gitTimer);
